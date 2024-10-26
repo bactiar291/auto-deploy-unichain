7850bcc5c67408d7076d2eb2c8d576de40391bd9
bactiar291 <134827499+bactiar291@users.noreply.github.com>
2024-10-26 17:28:47 +0700

    Update unichain_sepolia.py

--- unichain_sepolia.py
+++ unichain_sepolia.py
@@ -1,37 +1,34 @@
+import os
 import time
 from web3 import Web3
 from solcx import install_solc, set_solc_version, compile_source
 from colorama import init, Fore, Style
 import emoji
 from dotenv import load_dotenv
-import os
-
 
 load_dotenv()
 
-
 init(autoreset=True)
 
+os.system('cls' if os.name == 'nt' else 'clear')
 
-print(Fore.YELLOW +                        "========================================")
-print(Fore.CYAN +                                 "AUTHOR : ANAM BACTIAR")
-print(Fore.MAGENTA +                            "THANKS TO : ANAM BACTIAR!")
-print(Fore.BLUE +                         "GITHUB: https://github.com/bactiar291")
-print(Fore.GREEN +             "BUY COFFE FOR ME : 0x648dce97a403468dfc02c793c2b441193fccf77b ")
-print(Fore.YELLOW +                      "========================================\n")
+def display_banner():
+    print(Fore.YELLOW + "========================================")
+    print(Fore.CYAN + emoji.emojize(":rocket:") + " AUTHOR : ANAM BACTIAR")
+    print(Fore.MAGENTA + emoji.emojize(":star:") + " THANKS TO : ANAM BACTIAR!")
+    print(Fore.BLUE + emoji.emojize(":globe_with_meridians:") + " GITHUB: https://github.com/bactiar291")
+    print(Fore.GREEN + emoji.emojize(":coffee:") + " BUY COFFEE FOR ME: 0x648dce97a403468dfc02c793c2b441193fccf77b ")
+    print(Fore.YELLOW + "========================================\n")
 
+display_banner()
 
-install_solc('0.8.0')  # Menginstal Solidity versi 0.8.0
-set_solc_version('0.8.0')  # Atur versi Solidity yang akan digunakan
-
+install_solc('0.8.0')
+set_solc_version('0.8.0')
 
 infura_url = os.getenv('INFURA_URL')
 web3 = Web3(Web3.HTTPProvider(infura_url))
-
-
 account_address = os.getenv('ACCOUNT_ADDRESS')
 private_key = os.getenv('PRIVATE_KEY')
-
 
 contract_source_code = '''
 pragma solidity ^0.8.0;
@@ -53,55 +50,37 @@ contract SimpleStorage {
 }
 '''
 
-
 def deploy_contract():
     try:
-       
         compiled_sol = compile_source(contract_source_code)
         contract_interface = compiled_sol['<stdin>:SimpleStorage']
-
-       
         abi = contract_interface['abi']
         bytecode = contract_interface['bin']
-
-       
         SimpleStorage = web3.eth.contract(abi=abi, bytecode=bytecode)
-
-       
         gas_estimate = SimpleStorage.constructor().estimate_gas()
-
-       
         transaction = SimpleStorage.constructor().build_transaction({
             'from': account_address,
             'nonce': web3.eth.get_transaction_count(account_address),
             'gas': gas_estimate,
             'gasPrice': web3.eth.gas_price
         })
-
-       
         signed_tx = web3.eth.account.sign_transaction(transaction, private_key)
-
-       
         tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
-
-       
         tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
         contract_address = tx_receipt.contractAddress
-
-       
-        print(Fore.GREEN + emoji.emojize(':check_mark_button:') + f' Kontrak berhasil di-deploy di alamat: {contract_address}')
-
+        print(Fore.GREEN + emoji.emojize(":check_mark_button:") + f' Kontrak berhasil di-deploy di alamat: {contract_address}')
     except Exception as e:
-       
-        print(Fore.RED + emoji.emojize(':cross_mark:') + f' Gagal deploy kontrak: {str(e)}')
-
+        print(Fore.RED + emoji.emojize(":cross_mark:") + f' Gagal deploy kontrak: {str(e)}')
 
 while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    display_banner()
     if web3.is_connected():
         print(Fore.BLUE + "Memulai proses deploy...")
         deploy_contract()
     else:
-        print(Fore.RED + emoji.emojize(':cross_mark:') + " Tidak dapat terhubung ke node Ethereum")
-
-   
-    time.sleep(10)
+        print(Fore.RED + emoji.emojize(":cross_mark:") + " Tidak dapat terhubung ke node Ethereum")
+    for i in range(10, 0, -1):
+        print(Fore.MAGENTA + f"Menunggu {i} detik untuk mencoba kembali...", end='\r')
+        time.sleep(1)
+    print("\n" + Fore.GREEN + "Memulai kembali proses...\n")
